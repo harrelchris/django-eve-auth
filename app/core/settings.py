3c608ede205ba99cd99748b87edefdcd8a5633f2
Chris Harrel <harrelchris@gmail.com>
2024-06-21 20:46:49 -0500

    Add static assets and update templates

--- app/core/settings.py
+++ app/core/settings.py
@@ -86,6 +86,12 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+STATIC_ROOT = BASE_DIR.parent / ".static/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static/",
+]
+
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "users.User"
