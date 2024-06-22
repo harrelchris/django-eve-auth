6b400bade71e795908c82494d77d0e3c6eaa7a75
Chris Harrel <harrelchris@gmail.com>
2024-06-22 09:39:52 -0500

    Add refresh method

--- app/sso/models.py
+++ app/sso/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.utils import timezone
 
+from sso import services
+
 
 class Token(models.Model):
     character_id = models.IntegerField()
@@ -11,3 +13,18 @@ class Token(models.Model):
 
     def __str__(self):
         return f"{self.character_name} - {self.expiration}"
+
+    @property
+    def _is_expired(self):
+        return self.expiration < timezone.now()
+
+    def _refresh_access_token(self):
+        oauth_token = services.refresh_oauth_token(refresh_token=self.refresh_token)
+        self.access_token = oauth_token["access_token"]
+        self.expiration = oauth_token["expiration"]
+        self.save()
+
+    def get_access_token(self):
+        if self._is_expired:
+            self._refresh_access_token()
+        return self.access_token
